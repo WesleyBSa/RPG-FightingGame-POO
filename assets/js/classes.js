dd8fb279dac5f0c021d82a5c43bc331fa8752382
Wesley Santana <88838006+WesleyBSa@users.noreply.github.com>
2024-03-04 12:35:05 -0300

    Add files via upload
    Adicionado:
    Scroll automático na aba de log de evento.
    Efeito de opacidade no personagem morto.
    Botão de reinicio de partida após um dos personagens morrer.
    Imagens na pasta para novos personagens que serão implementados.

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -3,7 +3,7 @@
 
 class Character {
 
-    _life = 1;
+    _life = 1; //Tanto o life quando o maxLife começam com valores "1" pois zerado os personagens estariam mortos xD.
     maxLife = 1;
     attack = 0;
     defense = 0;
@@ -16,10 +16,11 @@ class Character {
         return this._life;
     }
     set life(newLife) {
-        this._life = newLife < 0 ? 0 : newLife;
+        this._life = newLife < 0 ? 0 : newLife; //Por padrão, após levar um ataque que faça o personagem ir a 0 ou  abaixo de 0, o life fica setado para o minimo que é o próprio 0.
     }
 }
 
+//Classe com caracteristicas do guerreiro
 class Knight extends Character {
     constructor(name) {
         super(name);
@@ -30,6 +31,7 @@ class Knight extends Character {
     }
 }
 
+//Classe com caracteristicas do mago
 class Sorcerer extends Character {
     constructor(name) {
         super(name);
@@ -40,6 +42,7 @@ class Sorcerer extends Character {
     }
 }
 
+//Classe com caracteristicas do little monster
 class LittleMonster extends Character {
     constructor() {
         super('Little Monster');
@@ -50,6 +53,7 @@ class LittleMonster extends Character {
     }
 }
 
+//Classe com caracteristicas do big monster
 class BigMonster extends Character {
     constructor() {
         super('Big Monster');
@@ -60,8 +64,11 @@ class BigMonster extends Character {
     }
 }
 
+// --------------------------------------------------------------------------------------
+
+//Classe cenário
 class Stage {
-    constructor(fighter1, fighter2, fighter1El, fighter2El, logObject) {
+    constructor(fighter1, fighter2, fighter1El, fighter2El, logObject) { //Nesse constructor precisamos passar os 4 valores: Os 2 lutadores e os seus respectivos elementos.
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
         this.fighter1El = fighter1El;
@@ -71,46 +78,51 @@ class Stage {
 
     start() {
         this.update();
+        //TODO: Evento do botão de atacar.
+
         this.fighter1El.querySelector('.attackbutton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+
         this.fighter2El.querySelector('.attackbutton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
     }
 
     update() {
+        //fighter 1
         this.updateCharacterView(this.fighter1, this.fighter1El);
+        //fighter 2
         this.updateCharacterView(this.fighter2, this.fighter2El);
     }
 
     updateCharacterView(character, element) {
         element.querySelector('.name').innerHTML = `${character.name} - ${character.life.toFixed(1)} HP`;
-        let pct = (character.life / character.maxLife) * 100;
-        element.querySelector('.lifebar .bar').style.width = `${pct}%`;
+        let pct = (character.life / character.maxLife) * 100; //Calculo para a barra de vida do personagem.
+        element.querySelector('.lifebar .bar').style.width = `${pct}%`; //Pega o elemento, adiciona no seu estilo o 100% na largura, preenchendo assim toda a barra de vida.
     }
 
     doAttack(attacking, attacked) {
-        if (attacking.life <= 0 || attacked.life <= 0) {
+        if (attacking.life <= 0 || attacked.life <= 0) { //Condicional para identificar se um dos atacados chegou a 0 de hp, chegando no indice 0 o personagem está morto.
             this.log.addMessage(`Está morto`);
             this.checkVictory();
             return;
         }
 
-        let attackFactor = (Math.random() * 2).toFixed(2);
+        let attackFactor = (Math.random() * 2).toFixed(2); //Fator aleatório para a força de ataque, utilizando o Math.random() * 2 permitimos que os valores aletórios seja no máximo o dobro do parametro padrão.
         let defenseFactor = (Math.random() * 2).toFixed(2);
 
         let actualAttack = attacking.attack * attackFactor;
         let actualDefense = attacked.defense * defenseFactor;
 
-        if (actualAttack > actualDefense) {
-            attacked.life -= actualAttack;
+        if (actualAttack > actualDefense) { //Condicional para caso o  índice de ataque atual seja maior que o índice de defesa o atacado receba dano.
+            attacked.life -= actualAttack; //Reduz a vida do atacado.
             this.log.addMessage(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`);
             
-            // Adicionando animação visual de ataque ao lutador que está sendo atacado
+            // Adicionando animação visual de ataque ao lutador que está sendo atacado.
             this.showAttackAnimation(attacked);
         } else {
             this.log.addMessage(`${attacked.name} conseguiu defender.`);
         }
 
-        this.update();
-        this.checkVictory();
+        this.update(); //Atualiza o placar.
+        this.checkVictory(); //Checa o vencedor.
     }
 
     showAttackAnimation(attacked) {
@@ -118,30 +130,43 @@ class Stage {
 
         attackedEl.classList.add('attack-animation');
 
-        // Remova a classe após um curto período para que a animação não permaneça indefinidamente
+        //Remove a classe após um curto período para que a animação não permaneça indefinidamente
         setTimeout(() => {
             attackedEl.classList.remove('attack-animation');
-        }, 1000); // Ajuste o tempo conforme necessário
+        }, 1000); //Tempo de animação
     }
 
     checkVictory() {
         if (this.fighter1.life <= 0) {
-            this.log.addMessage(`${LittleMonster} venceu!`);
+            this.log.addMessage(`Você perdeu!`);
             this.showWinnerMessage(this.fighter2.name);
             this.disableButtons();
+            this.enableRestartButton();
+    
+            // Adiciona a classe 'dead' à imagem do personagem
+            this.fighter1El.querySelector('.character-image').classList.add('dead');
         } else if (this.fighter2.life <= 0) {
-            this.log.addMessage(`${'Guerreiro'} venceu`);
+            this.log.addMessage(`Você venceu!`);
             this.showWinnerMessage(this.fighter1.name);
             this.disableButtons();
+            this.enableRestartButton();
+    
+            // Adiciona a classe 'dead' à imagem do monstro
+            this.fighter2El.querySelector('.character-image').classList.add('dead');
         }
     }
+    
 
-    disableButtons() {
+    disableButtons() { //Desabilita a opção de atacar quando um dos personagens morre.
         this.fighter1El.querySelector('.attackbutton').disabled = true;
         this.fighter2El.querySelector('.attackbutton').disabled = true;
     }
+
+    enableRestartButton() { //Habilita o botão de reiniciar
+        document.querySelector('.restart-button').disabled = false;
+    }
     
-    showWinnerMessage(winnerName) {
+    showWinnerMessage(winnerName) { //Apresenta o vencedor no final.
         const winnerMessage = document.querySelector('.winner-message');
         winnerMessage.textContent = `${winnerName} Wins!!!`;
         winnerMessage.style.display = 'block';
@@ -162,11 +187,12 @@ class Log {
     }
 
     render() {
-        this.listEl.innerHTML = '';
+        this.listEl.innerHTML = ''; //Limpa a lista HTML.
 
         for (let i in this.list) {
-            this.listEl.innerHTML += `<li><span>🗡</span>${this.list[i]}</li>`;
+            this.listEl.innerHTML += `<li><span>🗡</span>${this.list[i]}</li>`; //Incrementa na lista as mensagens do log. de eventos.
         }
+        this.listEl.scrollTop = this.listEl.scrollHeight; //Auto Scroll no log de eventos.
     }
 
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,35 @@ const stage = new Stage (
     log
 );
 
+function restartGame() {
+    //Habilita os botões de ataque
+    stage.fighter1El.querySelector('.attackbutton').disabled = false;
+    stage.fighter2El.querySelector('.attackbutton').disabled = false;
+
+    //Restaura os personagens para os valores iniciais
+    char.life = char.maxLife;
+    monster.life = monster.maxLife;
+
+    //Reinicia o objeto de log
+    log = new Log(document.querySelector('.log'));
+
+    //Reinicia o objeto de cenário
+    stage.log = log;
+    stage.update();
+
+    //Oculta a mensagem do vencedor
+    document.querySelector('.winner-message').style.display = 'none';
+
+    //Desabilita o botão de reiniciar
+    document.querySelector('.restart-button').disabled = true;
+
+    // Remove a classe 'dead' da imagem do personagem e do monstro
+    document.querySelector('#char .character-image').classList.remove('dead');
+    document.querySelector('#monster .character-image').classList.remove('dead');
+}
+
 stage.start();
+
 
 
 
